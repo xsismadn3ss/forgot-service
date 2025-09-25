cfb31a406643993ce77fc13d8da8616d55fb20a6
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-25 16:09:50 -0600

    iniciar repositorio

---
+++ src/main/java/com/grupo3/forgotservice/ForgotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.forgotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgotServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo3/forgotservice/ForgotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.forgotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
