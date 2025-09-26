d3f06c638d7ba51c24ba1434ea9979f75cb123db
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-26 16:43:39 -0600

    habilitar caching y clientes con feign

--- src/main/java/com/grupo3/forgotservice/ForgotServiceApplication.java
+++ src/main/java/com/grupo3/forgotservice/ForgotServiceApplication.java
@@ -2,8 +2,12 @@ package com.grupo3.forgotservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
+@EnableFeignClients
 public class ForgotServiceApplication {
 
     public static void main(String[] args) {
