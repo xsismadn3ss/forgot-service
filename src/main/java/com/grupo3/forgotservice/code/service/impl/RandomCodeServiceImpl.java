b777bbf67e3d1b112917be22321b8418ab9eba0f
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-26 22:03:35 -0600

    cambiar scope para que se cree una instancia por peticion y que no sea singleton

--- src/main/java/com/grupo3/forgotservice/code/service/impl/RandomCodeServiceImpl.java
+++ src/main/java/com/grupo3/forgotservice/code/service/impl/RandomCodeServiceImpl.java
@@ -1,11 +1,15 @@
 package com.grupo3.forgotservice.code.service.impl;
 
 import com.grupo3.forgotservice.code.service.IRandomCodeService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Random;
 
 @Service
+@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class RandomCodeServiceImpl implements IRandomCodeService {
     private static final Random random = new Random();
 
